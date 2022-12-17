2b8ca6785d8cc34847b18a9fc1a1263ac6a46551
Doug Hauenstein <doug.hauenstein@gmail.com>
2022-12-17 10:31:09 -0500

    Base Chart Config

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,7 +1,7 @@
 const open = require("open");
 const path = require("path");
 const server = "$";
-const file = "jsDev";
+const file = "ArsBrosRugCleaing";
 const uploadScript = "UploadToHTML";
 
 const fileUrl = `fmp://${server}/${file}?script=${uploadScript}&param=`;
@@ -9,3 +9,4 @@ const fileUrl = `fmp://${server}/${file}?script=${uploadScript}&param=`;
 const thePath = path.join(__dirname, "../", "dist", "index.html");
 const url = fileUrl + encodeURIComponent(thePath);
 open(url);
+ß
